cdf905172fad21f9dcd769ffeda2190ed6f161a7
Max Miller <maxmiller413@gmail.com>
2021-01-30 04:18:59 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,10 +5,10 @@ def key_for_min_value(name_hash)
   min_key = nil 
   min_value = nil 
   name_hash.each do |k, v|
-    if k == min_key
+    if v == min_value 
       min_key = k 
       min_value = v 
-    elsif k < min_key
+    elsif v < min_value 
       min_key = k 
       min_value = v 
     end 
