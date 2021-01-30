f1b1c448ac0518a10a66299bddc27c06dd9d2886
Max Miller <maxmiller413@gmail.com>
2021-01-30 04:17:47 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,13 +4,13 @@
 def key_for_min_value(name_hash)
   min_key = nil 
   min_value = nil 
-  name_hash.each do |key, value|
-    if min_value == nil 
-      min_value = value 
-      min_key = key 
-    elsif value < min_value
-      min_value = value
-      min_key = key 
+  name_hash.each do |k, v|
+    if k == min_key
+      min_key = k 
+      min_value = v 
+    elsif k < min_key
+      min_key = k 
+      min_value = v 
     end 
   end 
   min_key
